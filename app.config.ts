80cf70ec6a92e31d3b919b288fc7a4d26df18c32
Esteban Saá <steban@gmail.com>
2023-06-15 22:19:54 -0400

    fixes

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'tokenID',
+    description: 'Enhancing ERC20 Tokens with Off-Chain Metadata.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxtstudio',
@@ -32,7 +32,7 @@ export default defineAppConfig({
     footer: {
       iconLinks: [
         {
-          href: 'https://nuxt.com',
+          href: 'https://twitter.com/tokenid',
           icon: 'simple-icons:nuxtdotjs'
         }
       ]
